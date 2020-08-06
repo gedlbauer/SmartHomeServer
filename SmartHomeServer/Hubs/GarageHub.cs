7483bf8094585c2997a69fe6f9871ef6a021e35b
Georg Edlbauer <edlbauerg@gmail.com>
2020-08-06 10:43:47 +0200

    initial commit; SignalR hub without authentication

---
+++ SmartHomeServer/Hubs/GarageHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHomeServer.Hubs
+{
+    public class GarageHub : Hub
+    {
+        public async Task SendState(string state)
+        {
+            Console.WriteLine($"state changed to {state}");
+            await Clients.All.SendAsync("ReceiveState", state);
+        }
+
+        public async Task RegisterClient()
+        {
+            Console.WriteLine(Context.ConnectionId);
+        }
+
+    }
+}
